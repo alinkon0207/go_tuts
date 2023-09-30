3bcb8fb6fcba02b792f71d8cbadf6fe6d2624e05
ilesoviy <ilisovij861@gmail.com>
2023-09-30 23:32:49 +0900

    Return greetings for multiple people

--- Section 1/greetings/greetings.go
+++ Section 1/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -12,7 +13,22 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// Create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := [] string {
+		"Hi, %v. Welcome!", 
+		"Great to see you, %v!", 
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
