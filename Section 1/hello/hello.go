1b8c6e5da7ee6f6b3d47e3aca0e8f4e96358fc69
ilesoviy <ilisovij861@gmail.com>
2023-09-30 23:58:11 +0900

    Return greetings for multiple people

--- Section 1/hello/hello.go
+++ Section 1/hello/hello.go
@@ -14,15 +14,18 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-	// Request a greeting message.
-    message, err := greetings.Hello("Gladys")
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
         log.Fatal(err)
     }
 
-	// If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+	// If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
